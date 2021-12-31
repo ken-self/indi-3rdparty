1ae68b32000e07148961232b23a8adba0f64e9bb
Ken Self <ken.kgself@gmail.com>
2021-12-31 23:47:13 +1100

    Rename source files and retain old source for initial comparison

--- indi-avalon/indi_avalon.xml.cmake
+++ indi-avalon/indi_avalon.xml.cmake
@@ -2,7 +2,7 @@
 <driversList>
 <devGroup group="Telescopes">
     <device label="Avalon StarGo" manufacturer="Avalon">
-        <driver name="LX200 StarGo">indi_lx200stargo</driver>
+        <driver name="Avalon StarGo">indi_stargo</driver>
         <version>@AVALON_VERSION_MAJOR@.@AVALON_VERSION_MINOR@</version>
     </device>
 </devGroup>
